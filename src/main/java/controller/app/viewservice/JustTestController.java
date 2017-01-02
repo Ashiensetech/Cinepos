3d2ad21008374595240f12ace6452f638b2ce2ba
Sarwar Sikder <Sarwar Sikder>
2017-01-02 18:23:44 +0600

    Russel Test

---
+++ src/main/java/controller/app/viewservice/JustTestController.java
@@ -0,0 +1,40 @@
+package controller.app.viewservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+public class JustTestController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public ModelAndView getData(){
+         List<String> list=getList();
+         ModelAndView model=new ModelAndView("index");
+         model.addObject("lists",list);
+
+         return model;
+
+    }
+
+    private List<String> getList(){
+
+        List<String> list=new ArrayList<String>();
+        list.add("List A");
+        list.add("List B");
+        list.add("List C");
+        list.add("List D");
+        list.add("List E");
+        list.add("List F");
+
+        return list;
+
+    }
+
+
+}
