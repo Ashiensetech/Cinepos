4d242f3e222c828cdab81795cfac2c347b6349f1
Sarwar Sikder <Sarwar Sikder>
2017-01-04 15:56:19 +0600

    distributor problem

--- src/main/java/dao/DistributorDao.java
+++ src/main/java/dao/DistributorDao.java
@@ -4,10 +4,12 @@ package dao;
 import entity.Distributor;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class DistributorDao extends BaseDao{
 
     public List<Distributor> getAll(){
