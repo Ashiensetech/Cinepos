206ae46d0391bf77b971be0dd1d2eed1e8b6d891
Sarwar Sikder <Sarwar Sikder>
2017-02-06 16:56:25 +0600

    selling insertion

--- src/main/java/controller/app/restservice/AppSellsService.java
+++ src/main/java/controller/app/restservice/AppSellsService.java
@@ -1,18 +1,27 @@
 package controller.app.restservice;
 
 import controller.app.AppUriPreFix;
+import dao.SellDetailsDao;
 import dao.SellsDao;
+import entity.Sells;
+import entity.SellsDetails;
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import utility.ServiceResponse;
+import validator.admin.AdminSellsService.CreateSells.CartForm;
 import validator.admin.AdminSellsService.CreateSells.CreateOrMergeSellingForm;
 import validator.admin.AdminSellsService.CreateSells.CreateOrMergeSellingFormValidator;
+import validator.admin.AdminSellsService.CreateSells.OrderForm;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Sarwar on 2/2/2017.
@@ -24,6 +33,8 @@ public class AppSellsService {
     SellsDao sellsDao;
     @Autowired
     CreateOrMergeSellingFormValidator createOrMergeSellingFormValidator;
+    @Autowired
+    SellDetailsDao sellDetailsDao;
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<?> create(@Valid CreateOrMergeSellingForm createOrMergeSellingForm,
@@ -55,13 +66,38 @@ public class AppSellsService {
         //System.out.println(createOrMergeSellingForm.orderForm.getTerminalId()+"Hello");
         System.out.println(createOrMergeSellingForm.orderForm.getCartForms());
 
+        Sells sells=new Sells();
+        sells.setSellingAmount(1.5);
+        sells.setSellingComment("Testing comment for sells");
+        sells.setCombo(true);
+        sells.setQuantity(5);
+        sells.setTerminalId(createOrMergeSellingForm.orderForm.getTerminalId());
+        sells.setStatus(true);
+        sells.setCreatedBy(1);
 
-        System.out.print(createOrMergeSellingForm.orderForm.getCartForms().size());
+        sellsDao.insert(sells);
 
 
+        List<SellsDetails> sellDetailsArray = new ArrayList<>();
 
+        List<CartForm> sellsDetailsCart=createOrMergeSellingForm.orderForm.getCartForms();
 
-        //System.out.println(createOrMergeSellingForm.getOrdersJson());
+        for(CartForm targetItem : sellsDetailsCart){
+
+            SellsDetails sellsDetails=new SellsDetails();
+
+            sellsDetails.setSellId(sells.getId());
+            sellsDetails.setConcessionProductId(targetItem.getId());
+            //sellsDetails.setComboId(1);
+           // sellsDetails.setSeatTypeId(1);
+            sellsDetails.setUserId(1);
+            sellsDetails.setUnitSellingAmount(targetItem.getPrice());
+            sellsDetails.setQuantity(targetItem.getQuantity());
+            sellsDetails.setSellingType(targetItem.getSellingType());
+            sellsDetails.setCreatedBy(1);
+
+            sellDetailsDao.insert(sellsDetails);
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(createOrMergeSellingForm.getOrdersJson());
 
--- src/main/java/validator/admin/AdminSellsService/CreateSells/CartForm.java
+++ src/main/java/validator/admin/AdminSellsService/CreateSells/CartForm.java
@@ -8,16 +8,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CartForm {
 
-     private int Id;
+     private Integer Id;
      private int quantity;
      private float price;
      private String sellingType;
 
-    public int getId() {
+    public Integer getId() {
         return Id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         Id = id;
     }
 
@@ -43,6 +43,28 @@ public class CartForm {
 
     public void setSellingType(String sellingType) {
         this.sellingType = sellingType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CartForm cartForm = (CartForm) o;
+
+        if (quantity != cartForm.quantity) return false;
+        if (Float.compare(cartForm.price, price) != 0) return false;
+        if (Id != null ? !Id.equals(cartForm.Id) : cartForm.Id != null) return false;
+        return sellingType != null ? sellingType.equals(cartForm.sellingType) : cartForm.sellingType == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Id != null ? Id.hashCode() : 0;
+        result = 31 * result + quantity;
+        result = 31 * result + (price != +0.0f ? Float.floatToIntBits(price) : 0);
+        result = 31 * result + (sellingType != null ? sellingType.hashCode() : 0);
+        return result;
     }
 
     @Override
