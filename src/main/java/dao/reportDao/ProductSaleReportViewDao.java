6878cd566516f7c59e61d5cda3e65e7abc5d41a2
Sarwar Sikder <Sarwar Sikder>
2017-02-07 17:11:31 +0600

    marging

--- src/main/java/dao/reportDao/ProductSaleReportViewDao.java
+++ src/main/java/dao/reportDao/ProductSaleReportViewDao.java
@@ -1,8 +1,7 @@
 package dao.reportDao;
 
 import dao.BaseDao;
-import entity.Combo;
-import entity.reportEntity.ProductSaleReportView;
+import entity.reportEntity.ProductReportView;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
@@ -10,17 +9,15 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Sarwar on 2/1/2017.
- */
+
 @Repository
 public class ProductSaleReportViewDao extends BaseDao {
 
-    public List<ProductSaleReportView> getAll(){
+    public List<ProductReportView> getAll(){
         Session session=this.sessionFactory.openSession();
 
         try{
-            return session.createQuery("FROM ProductSaleReportView").list();
+            return session.createQuery("FROM ProductReportView").list();
 
         }catch (HibernateException hEx){
             hEx.printStackTrace();
